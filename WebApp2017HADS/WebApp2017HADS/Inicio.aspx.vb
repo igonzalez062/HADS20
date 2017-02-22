dfa56372b8ed2ae3cc30fefb19d74d955b88d3e2
igonzalez062 <ikergonzalez86@gmail.com>
2017-02-22 18:24:48 +0100

    Version1

---
+++ WebApp2017HADS/WebApp2017HADS/Inicio.aspx.vb
@@ -0,0 +1,26 @@
+﻿Public Class Inicio
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub BotonLogin_Click(sender As Object, e As EventArgs) Handles BotonLogin.Click
+        Dim resultado As String
+
+        resultado = LibreriaFunciones.Funciones.loginUsuario(TextBoxEmail.Text, TextBoxContraseña.Text)
+        If resultado = "OK" Then
+            Response.Redirect("./AppUsuarioYaRegistrado.aspx")
+        ElseIf resultado = "ERRORDATOSINCORRECTOS" Then
+            LabelError.Text = "DATOS INCORRECTOS"
+        ElseIf resultado = "ERRORNOUSER" Then
+            LabelError.Text = "NO EXISTE ESE USUARIO"
+        ElseIf resultado = "ERRORAPERTURADECONEXION" Then
+            LabelError.Text = "ERROR EN LA APERTURA DE LA CONEXION"
+        End If
+    End Sub
+
+    Protected Sub LinkButtonOlvidadoContraseña_Click(sender As Object, e As EventArgs) Handles LinkButtonOlvidadoContraseña.Click
+
+    End Sub
+End Class
